99831ef8c416776a78da104631ec6de2e2bc4631
jero98772 <jero98772@protonmail.com>
2025-10-21 12:42:37 -0500

    Modify MyObject to concatenate values in __add__
    Updated the __add__ method to concatenate values as strings before converting to an integer. Added __repr__ and __str__ methods for better object representation.

--- src/dunders/dunders_minimal_case.py
+++ src/dunders/dunders_minimal_case.py
@@ -5,12 +5,21 @@ class MyObject:
         self.value = value
 
     def __add__(self, right_obj):
-        return MyObject(self.value + right_obj.value)
+        concatenated = str(self.value) + str(right_obj.value)
+        # Return as new MyObject with integer value from concatenation
+        return MyObject(int(concatenated))
+
+    def __repr__(self):
+        return f"MyObject({self.value})"
+
+    def __str__(self):
+        return str(self.value)
+
 
 if __name__ == '__main__':
     left_obj = MyObject(1)
-    print(left_obj)
     right_obj = MyObject(2)
-    print(left_obj)
-    print(right_obj)
-    print(left_obj + right_obj)
+    
+    print(left_obj)         # prints "1"
+    print(right_obj)        # prints "2"
+    print(left_obj + right_obj)  # prints "12"
